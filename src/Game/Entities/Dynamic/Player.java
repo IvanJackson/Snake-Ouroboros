400cde792b3f0073873cb6604fdfeb73187cdc4e
Wiliel Florenciani <wiliel.florenciani@upr.edu>
2018-09-08 20:43:07 -0400

    Changes the color of Snake and Background to different color

--- src/Game/Entities/Dynamic/Player.java
+++ src/Game/Entities/Dynamic/Player.java
@@ -1,5 +1,6 @@
 package Game.Entities.Dynamic;
 
+
 import Main.Handler;
 
 import java.awt.*;
@@ -104,7 +105,7 @@ public class Player {
         Random r = new Random();
         for (int i = 0; i < handler.getWorld().GridWidthHeightPixelCount; i++) {
             for (int j = 0; j < handler.getWorld().GridWidthHeightPixelCount; j++) {
-                g.setColor(Color.WHITE);
+                g.setColor(Color.MAGENTA);
 
                 if(playeLocation[i][j]||handler.getWorld().appleLocation[i][j]){
                     g.fillRect((i*handler.getWorld().GridPixelsize),
