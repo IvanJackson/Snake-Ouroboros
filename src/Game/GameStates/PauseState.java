dad4cf62671d0198a521676f866a7614a45c41ea
Wiliel Florenciani <wiliel.florenciani@upr.edu>
2018-09-28 16:47:52 -0400

    Placed Buttons in locations

--- src/Game/GameStates/PauseState.java
+++ src/Game/GameStates/PauseState.java
@@ -26,15 +26,15 @@ public class PauseState extends State {
             State.setState(handler.getGame().gameState);
         }));
 
-        uiManager.addObjects(new UIImageButton(56, 223+(64+16), 128, 64, Images.Options, () -> {
+        uiManager.addObjects(new UIImageButton(56, 223+(64+16), 128, 64, Images.BTitle, () -> {
             handler.getMouseManager().setUimanager(null);
             State.setState(handler.getGame().menuState);
         }));
 
-        uiManager.addObjects(new UIImageButton(56, (223+(64+16))+(64+16), 128, 64, Images.BTitle, () -> {
-            handler.getMouseManager().setUimanager(null);
-            State.setState(handler.getGame().menuState);
-        }));
+//        uiManager.addObjects(new UIImageButton(56, (223+(64+16))+(64+16), 128, 64, Images.BTitle, () -> {
+//            handler.getMouseManager().setUimanager(null);
+//            State.setState(handler.getGame().menuState);
+//        }));
 
 
 
